5130b126147d987efc68bea99c3cce9e4b1e04e7
Andy Butland <abutland73@gmail.com>
2022-01-10 13:02:08 +0100

    Aligned use of HttpClient with other integrations.

--- src/Umbraco.Cms.Integrations.Crm.Hubspot/Controllers/FormsController.cs
+++ src/Umbraco.Cms.Integrations.Crm.Hubspot/Controllers/FormsController.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
-using Umbraco.Cms.Integrations.Crm.Hubspot.Helpers;
 using Umbraco.Cms.Integrations.Crm.Hubspot.Models;
 using Umbraco.Cms.Integrations.Crm.Hubspot.Models.Dtos;
 using Umbraco.Web.Mvc;
@@ -13,14 +14,17 @@ namespace Umbraco.Cms.Integrations.Crm.Hubspot.Controllers
     [PluginController("UmbracoCmsIntegrationsCrmHubspot")]
     public class FormsController : UmbracoAuthorizedApiController
     {
+        // Using a static HttpClient (see: https://www.aspnetmonsters.com/2016/08/2016-08-27-httpclientwrong/).
+        private readonly static HttpClient s_client = new HttpClient();
+
+        // Access to the client within the class is via ClientFactory(), allowing us to mock the responses in tests.
+        internal static Func<HttpClient> ClientFactory = () => s_client;
+
         public async Task<List<HubspotFormDto>> GetAll()
         {
-            //TODO: Errorhandling
-            var httpClient = Http.GetHttpClient();
-
             var hubspotApiKey = ConfigurationManager.AppSettings["Umbraco.Cms.Integrations.Crm.Hubspot.ApiKey"];
 
-            var response = await httpClient.GetAsync("https://api.hubapi.com/forms/v2/forms?hapikey=" + hubspotApiKey);
+            var response = await ClientFactory().GetAsync("https://api.hubapi.com/forms/v2/forms?hapikey=" + hubspotApiKey);
             var forms = await response.Content.ReadAsStringAsync();
             var hubspotForms = HubspotForms.FromJson(forms);
 
