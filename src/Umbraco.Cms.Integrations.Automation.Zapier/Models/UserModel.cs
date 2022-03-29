01c3f5a1d73510776c5e07592ce03cb54e9bec50
Adrian Cojocariu <aco@umbraco.dk>
2022-03-29 14:11:31 +0300

    Zapier authentication controller.

---
+++ src/Umbraco.Cms.Integrations.Automation.Zapier/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Umbraco.Cms.Integrations.Automation.Zapier.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
