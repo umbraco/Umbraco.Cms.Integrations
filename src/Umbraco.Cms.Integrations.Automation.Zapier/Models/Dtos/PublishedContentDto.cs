2f33dd6be070e673858fc1ba02a6953c2f5e041d
Adrian Cojocariu <aco@umbraco.dk>
2022-04-20 16:56:25 +0300

    add missing dto

---
+++ src/Umbraco.Cms.Integrations.Automation.Zapier/Models/Dtos/PublishedContentDto.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Umbraco.Cms.Integrations.Automation.Zapier.Models.Dtos
+{
+    public class PublishedContentDto
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")] 
+        public string Name { get; set; }
+
+        [JsonProperty("publishDate")]
+        public string PublishDate { get; set; }
+    }
+}
