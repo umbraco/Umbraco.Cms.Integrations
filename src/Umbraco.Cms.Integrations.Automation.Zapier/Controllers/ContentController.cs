70f1a963f399855c7ecc513b00fb7a3ca5d2f03c
Adrian Cojocariu <aco@umbraco.dk>
2022-04-20 14:56:35 +0300

    Zapier - get content mock up controller

---
+++ src/Umbraco.Cms.Integrations.Automation.Zapier/Controllers/ContentController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+
+using System.Linq;
+
+using Umbraco.Cms.Integrations.Automation.Zapier.Configuration;
+using Umbraco.Cms.Integrations.Automation.Zapier.Models.Dtos;
+using Umbraco.Cms.Integrations.Automation.Zapier.Services;
+
+#if NETCOREAPP
+using Microsoft.Extensions.Options;
+
+using Umbraco.Cms.Web.Common.Controllers;
+using Umbraco.Cms.Core.Services;
+#else
+using System.Configuration;
+
+using Umbraco.Web.WebApi;
+using Umbraco.Core.Services;
+#endif
+
+namespace Umbraco.Cms.Integrations.Automation.Zapier.Controllers
+{
+    public class ContentController : UmbracoApiController
+    {
+        private readonly ZapierSettings Options;
+
+        private IContentService _contentService;
+
+        private readonly IUserValidationService _userValidationService;
+
+#if NETCOREAPP
+        public ContentController(IOptions<ZapierSettings> options, IContentService contentService, IUserValidationService userValidationService)
+#else
+        public ContentController(IContentService contentService, IUserValidationService userValidationService)
+#endif
+        {
+#if NETCOREAPP
+            Options = options.Value;
+#else
+            Options = new ZapierSettings(ConfigurationManager.AppSettings);
+#endif
+
+            _contentService = contentService;
+
+            _userValidationService = userValidationService;
+        }
+
+        public List<ContentTypeDto> Get()
+        {
+//            string username = string.Empty;
+//            string password = string.Empty;
+
+//#if NETCOREAPP
+//            if (Request.Headers.TryGetValue(Constants.ZapierAppConfiguration.UsernameHeaderKey,
+//                    out var usernameValues))
+//                username = usernameValues.First();
+//            if (Request.Headers.TryGetValue(Constants.ZapierAppConfiguration.PasswordHeaderKey,
+//                    out var passwordValues))
+//                password = passwordValues.First();
+//#else
+//            if (Request.Headers.TryGetValues(Constants.ZapierAppConfiguration.UsernameHeaderKey,
+//                    out var usernameValues))
+//                username = usernameValues.First();
+//            if (Request.Headers.TryGetValues(Constants.ZapierAppConfiguration.PasswordHeaderKey,
+//                    out var passwordValues))
+//                password = passwordValues.First();
+//#endif
+
+//            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password)) return null;
+
+//            var isAuthorized = _userValidationService.Validate(username, password, Options.UserGroup).GetAwaiter().GetResult();
+//            if (!isAuthorized) return null;
+
+            var root = _contentService.GetRootContent().Where(p => p.Published)
+                .OrderByDescending(p => p.PublishDate).FirstOrDefault();
+
+            return new List<ContentTypeDto>
+            {
+                new ContentTypeDto { Id = root.Id, Name = root.Name} 
+            };
+        }
+
+    }
+}
--- src/Umbraco.Cms.Integrations.Automation.Zapier/Models/Dtos/ContentTypeDto.cs
+++ src/Umbraco.Cms.Integrations.Automation.Zapier/Models/Dtos/ContentTypeDto.cs
@@ -4,6 +4,8 @@ namespace Umbraco.Cms.Integrations.Automation.Zapier.Models.Dtos
 {
     public class ContentTypeDto
     {
+        [JsonProperty("id")]
+        public int Id { get; set; }
 
         [JsonProperty("name")]
         public string Name { get; set; }
