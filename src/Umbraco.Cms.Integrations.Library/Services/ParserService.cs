b2a0fc26c3ba12390fcb503b7f077e776c853cc1
Adrian Cojocariu <aco@umbraco.dk>
2023-02-15 07:54:21 +0200

    Remove using.

--- src/Umbraco.Cms.Integrations.Library/Services/ParserService.cs
+++ src/Umbraco.Cms.Integrations.Library/Services/ParserService.cs
@@ -2,7 +2,6 @@
 using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Integrations.Library.Parsing;
-using static Umbraco.Cms.Core.Models.Property;
 
 namespace Umbraco.Cms.Integrations.Library.Services
 {
