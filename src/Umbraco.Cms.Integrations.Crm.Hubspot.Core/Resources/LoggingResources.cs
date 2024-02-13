cf47a640243344420d28a45d098f8afed925e4e3
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2024-02-13 11:46:59 +0100

    Update LoggingResources.cs

--- src/Umbraco.Cms.Integrations.Crm.Hubspot.Core/Resources/LoggingResources.cs
+++ src/Umbraco.Cms.Integrations.Crm.Hubspot.Core/Resources/LoggingResources.cs
@@ -4,8 +4,8 @@ namespace Umbraco.Cms.Integrations.Crm.Hubspot.Core.Resources
     public static class LoggingResources
     {
         public const string ApiFetchFormsFailed =
-            "Failed to fetch forms from Hubspot using API key: {0}";
+            "Failed to fetch forms from HubSpot using API key: {0}";
 
-        public const string OAuthFetchFormsFailed = "Failed to fetch forms from Hubspot using OAuth: {0}";
+        public const string OAuthFetchFormsFailed = "Failed to fetch forms from HubSpot using OAuth: {0}";
     }
 }
