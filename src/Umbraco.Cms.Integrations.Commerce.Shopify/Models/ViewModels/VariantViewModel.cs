9a8a79038b7e0292f3be9e209d68c8d71154f274
Rachel Breeze <rachel.breeze@nexergroup.com>
2024-06-11 19:55:11 +0100

    Added support for displaying prices and filtering on products picked from shopify

---
+++ src/Umbraco.Cms.Integrations.Commerce.Shopify/Models/ViewModels/VariantViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Umbraco.Cms.Integrations.Commerce.Shopify.Models.ViewModels
+{
+    public class VariantViewModel
+    {
+        public string Price { get; set; }
+        public string Sku { get; set; }
+        public int Position { get; set; }
+        public int InventoryQuantity { get; set; }
+        public bool Taxable { get; set; }
+
+    }
+}
