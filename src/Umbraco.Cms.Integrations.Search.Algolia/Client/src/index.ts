394d1b33a3dcc822d33c41f0af340fd0511586f6
Nathan Woulfe <nathan@nathanw.com.au>
2024-06-19 16:29:04 +1000

    no need for null-check

--- src/Umbraco.Cms.Integrations.Search.Algolia/Client/src/index.ts
+++ src/Umbraco.Cms.Integrations.Search.Algolia/Client/src/index.ts
@@ -5,19 +5,14 @@ import { OpenAPI } from "./api/index.ts";
 import { manifest as algoliaDashboardManifest } from "./dashboard/manifest.ts";
 import { manifest as algoliaContextManifest } from "./context/manifest.ts";
 
-
 export const onInit: UmbEntryPointOnInit = (host, extensionRegistry) => {
 
     extensionRegistry.registerMany([algoliaDashboardManifest, algoliaContextManifest]);
 
     host.consumeContext(UMB_AUTH_CONTEXT, async (instance) => {
-
-        if (!instance) return;
-
         const umbOpenApi = instance.getOpenApiConfiguration();
         OpenAPI.TOKEN = umbOpenApi.token;
         OpenAPI.BASE = umbOpenApi.base;
         OpenAPI.WITH_CREDENTIALS = true;
     });
-
 };
