64a5686c46fc41385bf7f9f6e2a555ddbce39db5
Nguyen Doan Thanh <145089181+dtng95@users.noreply.github.com>
2024-07-20 00:16:35 +0700

    V14 integrations: Shopify
    - Remove redundant project

--- src/Umbraco.Cms.Integrations.Commerce.Shopify.Core/Api/Configuration/BackOfficeSecurityRequirementsOperationFilter.cs
+++
@@ -1,8 +0,0 @@
-using Umbraco.Cms.Api.Management.OpenApi;
-
-namespace Umbraco.Cms.Integrations.Commerce.Shopify.Core.Api.Configuration;
-
-public class BackOfficeSecurityRequirementsOperationFilter : BackOfficeSecurityRequirementsOperationFilterBase
-{
-    protected override string ApiName => Constants.ManagementApi.ApiName;
-}
--- src/Umbraco.Cms.Integrations.Commerce.Shopify.Core/readme.md
+++
@@ -1,3 +0,0 @@
-# Umbraco.Cms.Integrations.Commerce.Shopify.Core
-
-Documentation for the __Shopify__ integration is available at the [Umbraco documentation website](https://docs.umbraco.com/umbraco-dxp/integrations/shopify).
