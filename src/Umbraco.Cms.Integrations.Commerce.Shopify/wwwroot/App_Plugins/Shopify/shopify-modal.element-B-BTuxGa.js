9e076f5366a98435c0b865a45b0c91c110e47bcf
Nguyen Doan Thanh <145089181+dtng95@users.noreply.github.com>
2024-07-22 23:13:02 +0700

    V14: Integrations (Shopify)
    - Remove redundant files

--- src/Umbraco.Cms.Integrations.Commerce.Shopify/wwwroot/App_Plugins/Shopify/shopify-modal.element-B-BTuxGa.js
+++
@@ -1,7 +0,0 @@
-import { UmbModalBaseElement as e } from "@umbraco-cms/backoffice/modal";
-class o extends e {
-}
-export {
-  o as default
-};
-//# sourceMappingURL=shopify-modal.element-B-BTuxGa.js.map
--- src/Umbraco.Cms.Integrations.Commerce.Shopify/wwwroot/App_Plugins/Shopify/shopify.js
+++
@@ -1,6 +0,0 @@
-import "@umbraco-cms/backoffice/auth";
-import { o as i } from "./index-DM1PFbxq.js";
-export {
-  i as onInit
-};
-//# sourceMappingURL=shopify.js.map
