4b3ab845620385a65fc2b228bbd51add89433a97
Nguyen Doan Thanh <145089181+dtng95@users.noreply.github.com>
2024-08-14 11:01:51 +0700

    V14: Integrations (ActiveCampaign)
    - First push

---
+++ src/Umbraco.Cms.Integrations.Crm.ActiveCampaign/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Umbraco.Cms.Integrations.Crm.ActiveCampaign.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
