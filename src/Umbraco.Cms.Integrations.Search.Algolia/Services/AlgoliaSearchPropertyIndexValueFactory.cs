020d5a5ba658b33a36aa2b457e008c17a542d1f3
Adrian Cojocariu <aco@umbraco.dk>
2024-09-09 18:39:44 +0300

    Toggle PropertyIndexValueFactory between deprecated methods; Minor UI fix.

--- src/Umbraco.Cms.Integrations.Search.Algolia/Services/AlgoliaSearchPropertyIndexValueFactory.cs
+++ src/Umbraco.Cms.Integrations.Search.Algolia/Services/AlgoliaSearchPropertyIndexValueFactory.cs
@@ -1,6 +1,6 @@
 ﻿using Umbraco.Cms.Core.Models;
-using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Cms.Core.PropertyEditors;
+using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Integrations.Search.Algolia.Providers;
 
 namespace Umbraco.Cms.Integrations.Search.Algolia.Services
@@ -8,20 +8,28 @@ namespace Umbraco.Cms.Integrations.Search.Algolia.Services
     public class AlgoliaSearchPropertyIndexValueFactory : IAlgoliaSearchPropertyIndexValueFactory
     {
         private readonly PropertyEditorCollection _propertyEditorsCollection;
-
         private readonly ConverterCollection _converterCollection;
+
+        private readonly ILocalizationService _localizationService;
+        private readonly IContentTypeService _contentTypeService;
 
         public AlgoliaSearchPropertyIndexValueFactory(
             PropertyEditorCollection propertyEditorCollection,
-            ConverterCollection converterCollection)
+            ConverterCollection converterCollection,
+            ILocalizationService localizationService,
+            IContentTypeService contentTypeService)
         {
             _propertyEditorsCollection = propertyEditorCollection;
-
             _converterCollection = converterCollection;
+            _localizationService = localizationService;
+            _contentTypeService = contentTypeService;
         }
 
         public virtual KeyValuePair<string, object> GetValue(IProperty property, string culture)
         {
+            var availableCultures = _localizationService.GetAllLanguages().Select(p => p.IsoCode);
+            IDictionary<Guid, IContentType> contentTypeDictionary = _contentTypeService.GetAll().ToDictionary(x => x.Key);
+
             var propertyEditor = _propertyEditorsCollection.FirstOrDefault(p => p.Alias == property.PropertyType.PropertyEditorAlias);
             if (propertyEditor == null)
             {
@@ -35,8 +43,22 @@ namespace Umbraco.Cms.Integrations.Search.Algolia.Services
                 return new KeyValuePair<string, object>(property.Alias, result);
             }
 
-            IEnumerable<KeyValuePair<string, IEnumerable<object>>> indexValues = propertyEditor.PropertyIndexValueFactory.GetIndexValues(property, culture, null, true);
-
+            IEnumerable<KeyValuePair<string, IEnumerable<object>>> indexValues =
+#if NET6_0 || NET7_0
+                propertyEditor.PropertyIndexValueFactory.GetIndexValues(
+                    property, 
+                    culture, 
+                    null, 
+                    true);
+#else
+                propertyEditor.PropertyIndexValueFactory.GetIndexValues(
+                    property,
+                    culture,
+                    null,
+                    true,
+                    availableCultures,
+                    contentTypeDictionary);
+#endif
             if (indexValues == null || !indexValues.Any()) return new KeyValuePair<string, object>(property.Alias, string.Empty);
 
             var indexValue = indexValues.First();
