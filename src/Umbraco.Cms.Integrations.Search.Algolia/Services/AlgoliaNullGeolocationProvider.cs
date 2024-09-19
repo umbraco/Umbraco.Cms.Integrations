d2fa67122d584d483bd6268fca4df82ec84fb9fc
Adrian Cojocariu <aco@umbraco.dk>
2024-09-19 17:14:19 +0300

    Pass content object to geolocation interface

--- src/Umbraco.Cms.Integrations.Search.Algolia/Services/AlgoliaNullGeolocationProvider.cs
+++ src/Umbraco.Cms.Integrations.Search.Algolia/Services/AlgoliaNullGeolocationProvider.cs
@@ -1,9 +1,10 @@
-﻿using Umbraco.Cms.Integrations.Search.Algolia.Models;
+﻿using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Integrations.Search.Algolia.Models;
 
 namespace Umbraco.Cms.Integrations.Search.Algolia.Services
 {
     public class AlgoliaNullGeolocationProvider : IAlgoliaGeolocationProvider
     {
-        public async Task<List<GeolocationEntity>> GetGeolocationAsync() => null;
+        public async Task<List<GeolocationEntity>> GetGeolocationAsync(IContent content) => null;
     }
 }
--- src/Umbraco.Cms.Integrations.Search.Algolia/Services/IAlgoliaGeolocationProvider.cs
+++ src/Umbraco.Cms.Integrations.Search.Algolia/Services/IAlgoliaGeolocationProvider.cs
@@ -1,9 +1,10 @@
-﻿using Umbraco.Cms.Integrations.Search.Algolia.Models;
+﻿using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Integrations.Search.Algolia.Models;
 
 namespace Umbraco.Cms.Integrations.Search.Algolia.Services
 {
     public interface IAlgoliaGeolocationProvider
     {
-        Task<List<GeolocationEntity>> GetGeolocationAsync();
+        Task<List<GeolocationEntity>> GetGeolocationAsync(IContent content);
     }
 }
