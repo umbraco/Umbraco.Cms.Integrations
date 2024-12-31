ccd35b62ccbe2e883cc97cb398d532af1c9612c8
Rachel Breeze <hello@rachelbreeze.dev>
2024-12-31 12:18:02 +0000

    Added support for Specifying Caching level

--- src/Umbraco.Cms.Integrations.Commerce.Shopify/Configuration/ShopifySettings.cs
+++ src/Umbraco.Cms.Integrations.Commerce.Shopify/Configuration/ShopifySettings.cs
@@ -1,4 +1,9 @@
 ﻿using System.Collections.Specialized;
+#if NETCOREAPP
+using Umbraco.Cms.Core.PropertyEditors;
+#else
+using Umbraco.Core.PropertyEditors;
+#endif
 
 namespace Umbraco.Cms.Integrations.Commerce.Shopify.Configuration
 {
@@ -6,7 +11,7 @@ namespace Umbraco.Cms.Integrations.Commerce.Shopify.Configuration
     {
         public ShopifySettings()
         {
-            
+
         }
 
         public ShopifySettings(NameValueCollection appSettings)
@@ -17,6 +22,26 @@ namespace Umbraco.Cms.Integrations.Commerce.Shopify.Configuration
             UseUmbracoAuthorization = bool.TryParse(appSettings[Constants.Configuration.UmbracoCmsIntegrationsCommerceShopifyUseUmbracoAuthorizationKey], out var key)
                ? key
                : true;
+
+            // Enum.TryParse introduced in .NET site is .NETFramework and .NET Core, same logic will work for both
+            switch (appSettings[Constants.Configuration.UmbracoCmsIntegrationsCommerceShopifyPropertyCacheLevel])
+            {
+                case "Unknown":
+                    CacheLevel = PropertyCacheLevel.Unknown;
+                    break;
+                case "Element":
+                    CacheLevel = PropertyCacheLevel.Element;
+                    break;
+                case "Elements":
+                    CacheLevel = PropertyCacheLevel.Elements;
+                    break;
+                case "None":
+                    CacheLevel = PropertyCacheLevel.None;
+                    break;
+                default:
+                    CacheLevel = PropertyCacheLevel.Snapshot;
+                    break;
+            }
         }
 
         public string ApiVersion { get; set; }
@@ -26,5 +51,7 @@ namespace Umbraco.Cms.Integrations.Commerce.Shopify.Configuration
         public string AccessToken { get; set; }
 
         public bool UseUmbracoAuthorization { get; set; } = true;
+
+        public PropertyCacheLevel CacheLevel { get; set; } = PropertyCacheLevel.Snapshot;
     }
 }
--- src/Umbraco.Cms.Integrations.Commerce.Shopify/Editors/ShopifyProductPickerValueConverter.cs
+++ src/Umbraco.Cms.Integrations.Commerce.Shopify/Editors/ShopifyProductPickerValueConverter.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Umbraco.Cms.Integrations.Commerce.Shopify.Models.ViewModels;
 using Umbraco.Cms.Integrations.Commerce.Shopify.Services;
+using Umbraco.Cms.Integrations.Commerce.Shopify.Configuration;
 
 #if NETCOREAPP
+using Microsoft.Extensions.Options;
 using Umbraco.Cms.Core.PropertyEditors;
 using Umbraco.Cms.Core.Models.PublishedContent;
 #else
 using Umbraco.Core.Models.PublishedContent;
 using Umbraco.Core.PropertyEditors;
+using System.Configuration;
 #endif
 
 namespace Umbraco.Cms.Integrations.Commerce.Shopify.Editors
@@ -18,9 +21,19 @@ namespace Umbraco.Cms.Integrations.Commerce.Shopify.Editors
     public class ShopifyProductPickerValueConverter : PropertyValueConverterBase
     {
         private readonly IShopifyService _apiService;
+        private readonly ShopifySettings _settings;
 
+#if NETCOREAPP
+        public ShopifyProductPickerValueConverter(IOptions<ShopifySettings> options, IShopifyService apiService)
+#else
         public ShopifyProductPickerValueConverter(IShopifyService apiService)
+#endif
         {
+#if NETCOREAPP
+            _settings = options.Value;
+#else
+            _settings = new ShopifySettings(ConfigurationManager.AppSettings);
+#endif
             _apiService = apiService;
         }
 
@@ -30,7 +43,7 @@ namespace Umbraco.Cms.Integrations.Commerce.Shopify.Editors
         public override Type GetPropertyValueType(IPublishedPropertyType propertyType) => typeof(List<ProductViewModel>);
 
         public override PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType) =>
-            PropertyCacheLevel.Snapshot;
+            _settings.CacheLevel;
 
         public override object ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object source,
             bool preview)
