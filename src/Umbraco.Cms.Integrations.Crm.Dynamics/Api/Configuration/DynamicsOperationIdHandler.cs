82c3d9ec02cc10cc4f0847f2ddf32d5dcfcaad97
Adrian Cojocariu <aco@umbraco.dk>
2025-06-26 15:21:19 +0300

    Update versioning

--- src/Umbraco.Cms.Integrations.Crm.Dynamics/Api/Configuration/DynamicsOperationIdHandler.cs
+++ src/Umbraco.Cms.Integrations.Crm.Dynamics/Api/Configuration/DynamicsOperationIdHandler.cs
@@ -2,11 +2,6 @@
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Umbraco.Cms.Api.Common.OpenApi;
 
 namespace Umbraco.Cms.Integrations.Crm.Dynamics.Api.Configuration
