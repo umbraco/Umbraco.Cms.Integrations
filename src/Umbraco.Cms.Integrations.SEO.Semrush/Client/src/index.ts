7d65aa765f05cd4ca269142ddc214816fe10e5dd
Adrian Cojocariu <aco@umbraco.dk>
2025-07-01 16:59:41 +0300

    Update auth client config and versioning

--- src/Umbraco.Cms.Integrations.SEO.Semrush/Client/src/index.ts
+++ src/Umbraco.Cms.Integrations.SEO.Semrush/Client/src/index.ts
@@ -1,6 +1,7 @@
 import type { UmbEntryPointOnInit } from "@umbraco-cms/backoffice/extension-api";
 import { UMB_AUTH_CONTEXT } from "@umbraco-cms/backoffice/auth";
 import { client } from "@umbraco-integrations/semrush/generated";
+import { umbHttpClient } from "@umbraco-cms/backoffice/http-client";
 import { manifest as semrushContext } from "./context/manifest";
 import { manifests as workspaceManifest } from "./workspace/manifests";
 import { manifest as modalManifest } from "./modal/manifest";
@@ -13,12 +14,8 @@ export const onInit: UmbEntryPointOnInit = (host, extensionRegistry) => {
     ]);
   
     host.consumeContext(UMB_AUTH_CONTEXT, async (auth) => {
-        const config = auth?.getOpenApiConfiguration();
+        if (!auth) return;
 
-        client.setConfig({
-            auth: config?.token ?? undefined,
-            baseUrl: config?.base ?? "",
-            credentials: config?.credentials ?? "same-origin",
-        });
+        client.setConfig(umbHttpClient.getConfig());
     });
   };
