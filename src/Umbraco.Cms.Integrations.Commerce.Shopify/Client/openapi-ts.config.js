aad182725c89f634f573b0d869c67f73d8e00e64
Adrian Cojocariu <aco@umbraco.dk>
2025-11-19 12:35:53 +0200

    Shopify integration updated for Umbraco 17.

--- src/Umbraco.Cms.Integrations.Commerce.Shopify/Client/openapi-ts.config.js
+++ src/Umbraco.Cms.Integrations.Commerce.Shopify/Client/openapi-ts.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
     logs: {
         level: 'debug',
     },
-    input: 'http://localhost:30450/umbraco/swagger/shopify-management/swagger.json',
+    input: 'http://localhost:28157/umbraco/swagger/shopify-management/swagger.json',
     output: {
         path: 'generated',
     },
