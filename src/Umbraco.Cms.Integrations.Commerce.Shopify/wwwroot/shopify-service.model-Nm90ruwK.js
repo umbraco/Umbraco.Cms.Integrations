792d664bd7d8059e527a69a27e172796b76b942b
Rick Butterfield <rcb@umbraco.dk>
2025-11-25 11:56:25 +0000

    Upgrade to Umbraco 17.0.0

---
+++ src/Umbraco.Cms.Integrations.Commerce.Shopify/wwwroot/shopify-service.model-Nm90ruwK.js
@@ -0,0 +1,10 @@
+const o = {
+  api: "An access token is configured and will be used to connect to your Shopify account.",
+  oauth: "No access token is configured. To connect to your Shopify account using OAuth click 'Connect', select your account and agree to the permissions.",
+  none: "No access token or OAuth configuration could be found. Please review your settings before continuing.",
+  oauthConnected: "OAuth is configured and an access token is available to connect to your Shopify account. To revoke, click 'Revoke'"
+};
+export {
+  o as C
+};
+//# sourceMappingURL=shopify-service.model-Nm90ruwK.js.map
