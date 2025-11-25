2a8bbc5088b88ddbb6a05175c5ad4d8e633ff44b
Rick Butterfield <rcb@umbraco.dk>
2025-11-25 11:59:32 +0000

    Upgrade to Umbraco 17.0.0

---
+++ src/Umbraco.Cms.Integrations.SEO.Semrush/wwwroot/semrush.js
@@ -0,0 +1,7 @@
+import "@umbraco-cms/backoffice/auth";
+import { o as m } from "./index-m2GyFaVf.js";
+import "@umbraco-cms/backoffice/http-client";
+export {
+  m as onInit
+};
+//# sourceMappingURL=semrush.js.map
