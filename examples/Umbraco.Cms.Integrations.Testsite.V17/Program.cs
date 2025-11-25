fa8177280a9fd3e14ac487ec2545843b3d5f02c1
Rick Butterfield <rcb@umbraco.dk>
2025-11-25 11:37:57 +0000

    Rebuild assets

--- examples/Umbraco.Cms.Integrations.Testsite.V17/Program.cs
+++ examples/Umbraco.Cms.Integrations.Testsite.V17/Program.cs
@@ -1,5 +1,9 @@
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+#if DEBUG
+builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
+#endif
+
 builder.CreateUmbracoBuilder()
     .AddBackOffice()
     .AddWebsite()
